62493658e2de64da2b2ff51446efa53b05aa87cb
jensengbg-matilda-smedman <59621651+jensengbg-matilda-smedman@users.noreply.github.com>
2021-11-22 13:51:37 +0100

    issue w/ default edit value

--- src/views/editActivity-view.tsx
+++ src/views/editActivity-view.tsx
@@ -5,28 +5,29 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import GetFromBackend from "../hooks/getFromBackend";
 import { db } from "../firebase-config";
-import { setDoc, doc, deleteDoc, getDoc } from "@firebase/firestore";
+import { setDoc, doc, deleteDoc } from "@firebase/firestore";
 
 const EditView: React.FC = () => {
     let { id } = useParams()
     const { docs } = GetFromBackend("activities");
     const [activity, setActivity] = useState(null)
-
+    const [topic, setTopic] = useState("")
+    const [location, setLocation] = useState("")
+    const [date, setDate] = useState("")
+    const [description, setDesc] = useState("")
+    const [name, setName] = useState("")
 
     useEffect(() => {
         const index = [...docs].find((obj) => { return obj.id === id })
         setActivity(index)
     }, [activity, docs, id])
 
-
-    console.log('this is id,', id)
-    const [topic, setTopic] = useState("")
-    const handleEdit = async (id) => {
+    const handleEdit = async () => {
         const docRef = doc(db, "activities", id);
-        const payload = { topic }
+        const payload = { topic, location, date, description, name}
         setDoc(docRef, payload);
     }
-    const handleCancel = async (id) => {
+    const handleCancel = async () => {
         const docRef = doc(db, "activities", id)
         await deleteDoc(docRef);
         console.log(docRef, 'this is docref')
@@ -39,12 +40,21 @@ const EditView: React.FC = () => {
                     <h2>Edit activity, {activity.name}</h2>
                 </section>
                 <article className="greenArt">
+                    <h4>Name:</h4>
+                    <input value={activity.name} 
+                    onChange={(e) => setName(e.target.value)}  type="text" />
                     <h4>Date:</h4>
+                    <input type="text" value={activity.date}
+                        onChange={(e) => setDate(e.target.value)} />
                     <h4>Location:</h4>
+                    <input type="text" value={activity.location}
+                        onChange={(e) => setLocation(e.target.value)} />
                     <h4>Topic:</h4>
-                    <input type="text" value={topic}
+                    <input type="text" value={activity.topic} 
                         onChange={(e) => setTopic(e.target.value)} />
                     <h4>Description:</h4>
+                    <input value={activity.description} 
+                    onChange={(e) => setDesc(e.target.value)}  type="text" />
                     <section className="buttonSection">
                         <button className="cancelBtn" onClick={handleCancel}>Cancel activity</button>
                         <button className="editBtn">Stop editing</button>
