b0321a87a1802385fa079cf4f8b1f38bd6f093b6
jensengbg-matilda-smedman <59621651+jensengbg-matilda-smedman@users.noreply.github.com>
2021-11-25 07:28:29 +0100

    trying to fix location

--- src/views/profile-view.tsx
+++ src/views/profile-view.tsx
@@ -4,24 +4,34 @@ import user from '../assets/user.png';
 import '../Styles/_profile.scss';
 import { Link } from "react-router-dom";
 import {useAuth} from '../hooks/authentication'
-import {logout} from '../firebase-config'
+import {logout, auth} from '../firebase-config'
+import {updateProfile} from 'firebase'
 
 const ProfileView: React.FC = () => {
     const currentUser = useAuth();
+    const handleSubmit = async e => {
+        e.preventDefault()
+        const updatedisplayName =  currentUser.updateProfile({
+            displayName: ''
+          })
+        const payload = { updatedisplayName};
+        console.log("The new ID is: " + updatedisplayName + currentUser.displayName);
+    }
+
     return (
         <div className="profileDiv">
             <article className="profileArt">
                <img className="userLogo" src={currentUser?.photoURL || user } alt="user" />
                <p className="clickable">Pick new profile photo</p>
             </article>
-            <section className="profileSect">
+            <form className="profileSect" onSubmit={handleSubmit}>
                 <p>Username: { currentUser?.displayName || currentUser?.email}</p>
                 <input type="text" />
-                <p>Change password: </p>
+                <p>Change password: </p> <button >change</button>
                 <p>Your upcoming activities</p>
                 <p>Your topics: </p>
                 <p>Add new topic: </p>
-            </section>
+            </form>
             <button>Save profile</button>
             <Link to="/login">
                 <button onClick={logout}>Sign out</button>
