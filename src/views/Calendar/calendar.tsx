d26cb5113dd0aca28c134d42d1c4b3247bd73fe6
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2021-12-02 11:40:20 +0100

    Trying a new caledar component and fixing serviceWorker

--- src/views/Calendar/calendar.tsx
+++ src/views/Calendar/calendar.tsx
@@ -1,8 +1,8 @@
 // @ts-nocheck
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import {format, getDay, parse, startOfWeek} from "date-fns"
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 import GetFromBackend from "../../hooks/getFromBackend";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./_calendar.scss";
@@ -11,19 +11,25 @@ import "./_calendar.scss";
 const CalendarView: React.FC = () => {
     const { docs } = GetFromBackend("activities");
     const history = useHistory()
-    const locales = {
-        "en-eu": require("date-fns/locale/en-GB")
+    const [events, setEvents] = useState([]);
+    
+    const goToEvent = (e) => {
+        console.log(e.event)
     }
-    const localizer = dateFnsLocalizer({
-        format, parse, startOfWeek, getDay, locales
-    })
 
-    const goToActivity = ({action, props}) => {
-        console.log(action)
-        console.log(props)
-
-        //history.push(`/chosen/${doc.id}`)
-    }
+    useEffect(() => {
+        if (docs) {
+            const item = [...docs].map((obj) => {
+                return {
+                    title: obj.title, 
+                    start: obj.start,
+                    end: obj.end,
+                    id: obj.id,
+                }
+            })
+            setEvents(item)
+        }
+    }, [docs])
 
     return (
         <div className="calendarDiv">
@@ -32,15 +38,13 @@ const CalendarView: React.FC = () => {
                     <button>Add new activity</button>
                 </Link>
             </section>
-            <Calendar 
-                localizer={localizer} 
-                events={docs} 
-                startAccessor="start" 
-                endAccessor="end" 
-                selectable
-                style={{ height: 500, margin: "100px" }}
-                onSelectSlot={goToActivity}
+            <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+                events={events}
+                eventClick={(e) => goToEvent(e)}
             />
+
         </div>
     );
 }
