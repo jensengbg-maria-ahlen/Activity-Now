8a41bd65c06e8183b82ac3ae4c8738564c7d9987
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2021-12-09 12:07:06 +0100

    Changed design on cookie acceptance

--- src/views/HomePage/home.tsx
+++ src/views/HomePage/home.tsx
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import React from "react";
+import { Link } from "react-router-dom";
 import { useAuth } from '../../hooks/authentication'
 import YourActivity from "../../Components/HomePage/yourActivities"
 import FollowedTopics from "../../Components/HomePage/followedTopics";
@@ -14,28 +15,28 @@ const Home: React.FC = () => {
     return (
         <div className="home">
             <h1 className="title title--h1">Welcome, <span className="title title--bold">{currentUser?.displayName}</span></h1>
+            <CookieConsent
+                location="bottom"
+                buttonText="I accept"
+                cookieName="ActivityToday Cookie"
+                disableStyles={true}
+                debug={true}
+                expires={150}
+                enableDeclineButton
+                onDecline={() => {
+                    console.log('no cookies, user declined');
+                }}
+            >
+                <div className="CookieConsent__content">
+                    <p className="paragraph paragraph--bold">We use cookies</p>
+                    <p className="paragraph paragraph--small">We use and analyze cookies in order to be able to optimize this page for you. <br /> By clicking <span className="paragraph--bold orange">"I accept"</span>, you approve our use of cookies.</p>
+                    <p className="paragraph paragraph--small">Read more about how we use cookies <Link to={"/cookie"}><span className="paragraph--bold">here.</span></Link></p>
+                </div>
+            </CookieConsent>
             <div className="home__content">
                 <YourActivity />
                 <FollowedTopics />
                 <HotTopics />
-                <CookieConsent
-                    location="bottom"
-                    buttonText="Accept cookies"
-                    cookieName="ActivityToday Cookie"
-                    style={{ background: "#2B373B"  }}
-                    debug={true}
-                    buttonStyle={{color: "#171E20", background: "#87BA77", fontSize: "13px" }}
-                    expires={150}
-                    enableDeclineButton
-                    declineButtonStyle= {{color: "#171E20", background: '#F75031' }}
-                    onDecline={() => {
-                        
-                      console.log('no cookies, user declined');
-                    }}
-                    className="paragraph paragraph--small"
-                >
-                    This website uses cookies to enhance the user experience.
-                </CookieConsent>
             </div>
         </div>
     );
