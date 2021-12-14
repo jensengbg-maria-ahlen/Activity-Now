95bfd5e7d8ed3e3e0fd46cae97777255eee2d520
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2021-12-14 09:43:14 +0100

    Update firestore settings, removed unused code, changed css.

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -7,17 +7,17 @@ const Footer: React.FC = () => {
     return (
         <div className="footer">
             <div className="footer__content">
-            <h3 className="title title--h3 title--bold title--no-spacing">Help</h3>
+            <h3 className="title title--h3 title--bold">Need help</h3>
                 <div>
-                    <a className="paragraph paragraph--small paragraph--no-spacing" href="tel:+4733378901">Call us</a>
+                    <a className="paragraph paragraph--small" href="tel:+4733378901">Call us</a>
                     <a className="paragraph paragraph--small paragraph--no-spacing" href = "mailto: abc@example.com">Send Email</a>
                 </div>
             </div>
             <div className="footer__content">
-                <h3 className="title title--h3 title--bold title--no-spacing">Information</h3>
+                <h3 className="title title--h3 title--bold">Information</h3>
                 <div>
                     <Link to="/cookie">
-                        <p className="paragraph paragraph--small paragraph--no-spacing" >Cookies</p>
+                        <p className="paragraph paragraph--small" >Cookies</p>
                     </Link>
                     <Link to="/aboutus">
                         <p className="paragraph paragraph--small paragraph--no-spacing">About us</p>
@@ -25,9 +25,9 @@ const Footer: React.FC = () => {
                 </div>
             </div>
             <div className="footer__content">
-                <h3 className="title title--h3 title--bold title--no-spacing">Follow us</h3>
+                <h3 className="title title--h3 title--bold">Follow us</h3>
                 <div>
-                <a className="paragraph paragraph--small paragraph--no-spacing" href="https://www.instagram.com/">Instagram</a>
+                <a className="paragraph paragraph--small" href="https://www.instagram.com/">Instagram</a>
                     <a className="paragraph paragraph--small paragraph--no-spacing" href="https://www.facebook.com/">Facebook</a>
                 </div>
             </div>
