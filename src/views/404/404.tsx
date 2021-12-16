dec6d0a51e3649e80669dc3eda099833d8cc61fa
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2021-12-16 11:07:23 +0100

    Added goback on 404 page

--- src/views/404/404.tsx
+++ src/views/404/404.tsx
@@ -1,16 +1,18 @@
 // @ts-nocheck
 import React from "react";
+import { useHistory } from "react-router-dom";
 import errorImg from "../../assets/404.jpg";
 import "./_not-found.scss";
 
 const NotFound: React.FC = () => {
+    const history = useHistory()
     return (
         <article className="not-found">
             <h1 className="title title--h1 title--bold">404 Page not found</h1>
             <div className="not-found__image">
                 <img src={errorImg} alt="404 error" />
             </div>
-            <button className="go-back-btn">Go Back</button>
+            <button className="go-back-btn" onClick={history.goBack}>Go Back</button>
         </article>
     )
 }
